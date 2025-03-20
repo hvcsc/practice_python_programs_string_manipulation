b24079ecb32c14298a832e0797226c635f2bb0ad
hvcsc <jrhcmll13@gmail.com>
2025-03-20 20:59:42 +0800

    pseudocode

---
+++ batch_5/5.3_all_capital.py
@@ -0,0 +1,9 @@
+"""Prog03: Create a program that ask the user to input their fullname.
+Print the input in all capital letter.
+Example:
+Input: Juan Dela Cruz
+Output: JUAN DELA CRUZ"""
+
+#ask name
+#upper case
+#print input

f5cb224ae420190daab2c004f9feb13172311c95
hvcsc <jrhcmll13@gmail.com>
2025-03-20 21:21:00 +0800

    pseudocode

---
+++ batch_5/5.8_character_count.py
@@ -0,0 +1,9 @@
+"""Prog08: Create a program that ask the user to input their fullname.
+Print the number of characters in the input.
+Example:
+Input: Juan Dela Cruz
+Output: 14"""
+
+#ask name
+#count character
+#print count

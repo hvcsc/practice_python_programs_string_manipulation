3697c5a55699054e519a824ce4e772cbfcfac6cd
hvcsc <jrhcmll13@gmail.com>
2025-03-20 21:04:03 +0800

    pseudocode

---
+++ batch_5/5.4_lower_case.py
@@ -0,0 +1,8 @@
+"""Prog04: Create a program that ask the user to input their fullname.
+Print the input in all lower case.
+Example:
+Input: Juan Dela Cruz
+Output: juan dela cruz"""
+
+#ask name and change to lower case
+#print lower case name

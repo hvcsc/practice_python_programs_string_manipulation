56a0f0116296ec98a09cd9103007f1ec0d55741d
hvcsc <jrhcmll13@gmail.com>
2025-03-20 21:26:57 +0800

    print pascal casing

--- batch_5/5.9_pascal_casing.py
+++ batch_5/5.9_pascal_casing.py
@@ -5,5 +5,10 @@ Input: jUAn DEla CrUZ
 Output: JuanDelaCruz"""
 
 #ask name
+fullname = input("Enter your fullname in incorrect casing: ")
+
 #change to pascal casing
+pascal = fullname.title().replace(" ", "")
+
 #print name
+print(pascal)

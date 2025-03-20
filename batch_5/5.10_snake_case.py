0a4d69178fd1eaf8fc015752951df29f98e5124a
hvcsc <jrhcmll13@gmail.com>
2025-03-20 21:29:15 +0800

    pseudocode

---
+++ batch_5/5.10_snake_case.py
@@ -0,0 +1,9 @@
+"""Prog10: Create a program that ask the user to input their fullname in
+incorrect casing. Print the input in snake case.
+Example:
+Input: jUAn DEla CrUZ
+Output: juan_dela_cruz"""
+
+#ask name
+#change space to underscore
+#print snake case

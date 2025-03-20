23134c553ffeb6115235f28f560f9fbbc206b1c3
hvcsc <jrhcmll13@gmail.com>
2025-03-20 20:10:31 +0800

    pseudocode

---
+++ batch_5/5.1_input_without_spaces.py
@@ -0,0 +1,9 @@
+"""Prog01: Create a program that ask the user to input their fullname with several space
+characters at the beginning. Print the input without the spaces in the beginning.
+Example:
+Input:         Juan Dela Cruz
+Output: Juan Dela Cruz"""
+
+#ask users input
+#remove spaces
+#print input without the spaces at the beginning
